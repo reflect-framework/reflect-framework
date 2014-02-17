1a014bd8fc9ccc6b16f4b4b9271165099d5a666c
ntenhoeve <ntenhoeve@gmail.com>
2014-02-17 13:59:51 +0100

    New Filter

---
+++ Introspect/src/nth/introspect/filter/EqualsFilter.java
@@ -0,0 +1,17 @@
+package nth.introspect.filter;
+
+public class EqualsFilter<T> implements Filter<T> {
+
+
+
+	private T objectToCompare;
+
+	public EqualsFilter(T objectToCompare) {
+		this.objectToCompare = objectToCompare;
+	}
+	
+	@Override
+	public boolean isMatch(T t) {
+		return t.equals(objectToCompare);
+	}
+}
