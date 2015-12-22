c252de9c8f4b953492653b9cac7fa043d9f4b5a8
ntenhoeve <nilstenhoeve@gmail.com>
2015-12-22 14:43:12 +0100

    Moved Introspect test classes from IntrospectJUnitTest IntrospectForJUnit/src/test/java and removed IntrospectJUnitTest project (see #38)

--- IntrospectJUnitTest/src/nth/introspect/junit/layer5provider/reflection/behavior/icon/forclass/IconModelForClass_AnnotationClassReferenceUriNotExist.java
+++
@@ -1,9 +0,0 @@
-package nth.introspect.junit.layer5provider.reflection.behavior.icon.forclass;
-
-import nth.introspect.layer5provider.reflection.behavior.icon.Icon;
-import nth.introspect.layer5provider.reflection.behavior.icon.IconUriClassResource;
-
-@Icon(iconURI=IconUriClassResource.SCHEMA+"://bogus/bogus.png")
-public class IconModelForClass_AnnotationClassReferenceUriNotExist {
-
-}
