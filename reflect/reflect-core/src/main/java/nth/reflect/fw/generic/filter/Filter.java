df2006707dff0e27e53e9179ea6ab7f842dc9c63
nilsth <nilsth@192.168.2.6>
2018-08-22 14:41:25 +0200

    Depricated filter (use collection streams)

--- reflect/reflect-core/src/main/java/nth/reflect/fw/generic/filter/Filter.java
+++ reflect/reflect-core/src/main/java/nth/reflect/fw/generic/filter/Filter.java
@@ -1,11 +1,12 @@
 package nth.reflect.fw.generic.filter;
 
 /**
- *  With Java 8 update comes streams and lambdas, solving this
+ * @deprecated With Java 8 update comes streams and lambdas, solving this
  *             problem with a simple one-liner:
+ * 
  *             <pre>
- *  List&lt;Person&gt; beerDrinkers = persons.stream().filter(p -&gt; p.getAge() &gt; 16).collect(Collectors.toList());
- * </pre>
+ *             List&lt;Person&gt; beerDrinkers = persons.stream().filter(p -&gt; p.getAge() &gt; 16).collect(Collectors.toList());
+ *             </pre>
  *
  * @param <T>
  */
