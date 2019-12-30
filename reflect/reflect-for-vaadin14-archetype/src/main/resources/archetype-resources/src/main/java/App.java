8e9a7e103272e819b45fa52a3ac040f842cadb57
nilsth <nilsth@OZLT401.meyn.nl>
2019-12-30 15:29:03 +0100

    First commit reflect-for-vaadin14-archetype (sandbox project)

---
+++ reflect/reflect-for-vaadin14-archetype/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $org.reflect.for.vaadin14.archetype;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
