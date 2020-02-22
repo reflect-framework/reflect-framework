463ab6c251bf8841efe32e39e24b4761239865de
ntenhoeve <nilstenhoeve@gmail.com>
2020-02-22 17:57:23 +0100

    Fixed #370

---
+++ reflect/reflect-core/src/main/java/nth/reflect/fw/layer5provider/stringconverter/abstractconverter/StringConverterFactory.java
@@ -0,0 +1,8 @@
+package nth.reflect.fw.layer5provider.stringconverter.abstractconverter;
+
+import nth.reflect.fw.generic.contractor.Contractor;
+
+public interface StringConverterFactory<MAKE_TYPE>
+		extends Contractor<MAKE_TYPE, nth.reflect.fw.layer5provider.reflection.info.type.TypeInfo> {
+
+}
