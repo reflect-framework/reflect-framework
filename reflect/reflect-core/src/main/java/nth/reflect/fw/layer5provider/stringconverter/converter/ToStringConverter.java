aa2bdea02c30d7a0e9de2d6dddc2063c137503df
ntenhoeve <nilstenhoeve@gmail.com>
2020-02-22 17:20:35 +0100

    Fixed #343

---
+++ reflect/reflect-core/src/main/java/nth/reflect/fw/layer5provider/stringconverter/converter/ToStringConverter.java
@@ -0,0 +1,12 @@
+package nth.reflect.fw.layer5provider.stringconverter.converter;
+
+public interface ToStringConverter<T> {
+
+	/**
+	 * Converts the object provided into its string form. Format of the returned
+	 * string is defined by the specific converter.
+	 * 
+	 * @return a string representation of the object passed in.
+	 */
+	public String toString(T value);
+}
