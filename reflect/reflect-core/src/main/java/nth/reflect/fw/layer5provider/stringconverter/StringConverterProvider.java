412d254859e8953f00ef5a730382032b612896df
ntenhoeve <nilstenhoeve@gmail.com>
2020-03-12 10:30:34 +0100

    Fixed #384

--- reflect/reflect-core/src/main/java/nth/reflect/fw/layer5provider/stringconverter/StringConverterProvider.java
+++ reflect/reflect-core/src/main/java/nth/reflect/fw/layer5provider/stringconverter/StringConverterProvider.java
@@ -2,6 +2,7 @@ package nth.reflect.fw.layer5provider.stringconverter;
 
 import java.text.Format;
 
+import nth.reflect.fw.ReflectApplication;
 import nth.reflect.fw.generic.contractor.DelegatingContractor;
 import nth.reflect.fw.layer5provider.Provider;
 import nth.reflect.fw.layer5provider.reflection.info.type.TypeInfo;
@@ -13,6 +14,13 @@ import nth.reflect.fw.layer5provider.stringconverter.generic.StringConverterFact
  * The {@link StringConverterProvider} is a {@link Provider} that provides a
  * {@link StringConverter} for a given {@link TypeInfo} and a given (optional)
  * {@link Format} pattern.
+ * <p>
+ * {@insert StringConverter}
+ * <p>
+ * You can append or override custom {@link StringConverter}s by overriding the
+ * {@link ReflectApplication#getStringConverterProvider()} method.
+ * <p>
+ * {@insert DefaultStringConverters}
  * 
  * @author nilsth
  *
