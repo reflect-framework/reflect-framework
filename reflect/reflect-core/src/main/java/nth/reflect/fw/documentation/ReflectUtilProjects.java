8044a93e31504b4cfca7a5f385d7b3f2bed205c5
ntenhoeve <nilstenhoeve@gmail.com>
2020-04-08 21:33:36 +0200

    Fixed #416

---
+++ reflect/reflect-core/src/main/java/nth/reflect/fw/documentation/ReflectUtilProjects.java
@@ -0,0 +1,19 @@
+package nth.reflect.fw.documentation;
+
+import nth.reflect.fw.ReflectFramework;
+
+/**
+ * {@link ReflectUtilProjects} are reusable Java components that are or can be
+ * used in ReflectApplications. Most {@link ReflectUtilProjects} do not require
+ * (have no dependencies with) the {@link ReflectFramework}.
+ * <p>
+ * <h2>reflect-util-maven-plugin</h2> {@insert ReflectUtilMavenPlugin}
+ * <p>
+ * <h2>reflect-util-java-archive-scanner</h2> {@insert JavaArchiveScanner}
+ * 
+ * @author nilsth
+ *
+ */
+public interface ReflectUtilProjects extends ReflectDocumentation {
+
+}
