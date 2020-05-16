e134b3092a3cf3eb7a33b5e507eec5a5da1f16f0
nilsth <nilsth@OZLT401.meyn.nl>
2020-05-16 23:25:52 +0200

    Fixed #425

--- reflect/reflect-framework-for-gui/src/main/java/nth/reflect/fw/gui/layer5provider/actionmethodexecution/result/ActionMethodResultHandlerClasses.java
+++ reflect/reflect-framework-for-gui/src/main/java/nth/reflect/fw/gui/layer5provider/actionmethodexecution/result/ActionMethodResultHandlerClasses.java
@@ -1,9 +1,5 @@
 package nth.reflect.fw.gui.layer5provider.actionmethodexecution.result;
 
-import nth.reflect.fw.layer1userinterface.controller.UserInterfaceController;
-import nth.reflect.fw.layer5provider.actionmethodexecution.ActionMethodResultHandler;
-import nth.reflect.fw.layer5provider.actionmethodexecution.result.depricated.DeprecatedActionMethodResultHandler;
-
 public abstract class ActionMethodResultHandlerClasses
 		extends nth.reflect.fw.layer5provider.actionmethodexecution.ActionMethodResultHandlerClasses {
 
@@ -12,7 +8,6 @@ public abstract class ActionMethodResultHandlerClasses
 	public ActionMethodResultHandlerClasses() {
 		super();
 		add(1, getDownloadStreamResultHandler());
-		add(getDepricatedActionMethodsClass());
 	}
 
 	public abstract Class<? extends nth.reflect.fw.layer5provider.actionmethodexecution.result.DownloadStreamResultHandler> getDownloadStreamResultHandler();
@@ -39,15 +34,6 @@ public abstract class ActionMethodResultHandlerClasses
 	@Override
 	public Class<? extends nth.reflect.fw.layer5provider.actionmethodexecution.result.StringConverterResultHandler> getStringConverterResultHandler() {
 		return StringConverterResultHandler.class;
-	}
-
-	/**
-	 * TODO remove this method and the {@link #reflectApplication} field when all
-	 * {@link UserInterfaceController} open... methods are moved to
-	 * {@link ActionMethodResultHandler}s
-	 */
-	public Class<DeprecatedActionMethodResultHandler> getDepricatedActionMethodsClass() {
-		return DeprecatedActionMethodResultHandler.class;
 	}
 
 }
